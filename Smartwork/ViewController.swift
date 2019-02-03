f3106a731a4deb9b2205c367b8d2024d3e3e2ba7
Craig Newcomb <crgnewcomb@gmail.com>
2019-02-02 23:18:41 -0600

    Initial Commit

---
+++ Smartwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smartwork
+//
+//  Created by Craig Newcomb on 2/2/19.
+//  Copyright © 2019 Craig Newcomb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
